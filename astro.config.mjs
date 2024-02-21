f558a0663dabc7f310ca1e51d2c3f81b250f4461
Stetsed <stetsed@selfhostable.net>
2024-02-21 10:20:40 +0100

    New AS website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://as.selfhostable.net',
-  integrations: [mdx(), sitemap(), tailwind()]
-});
+export default defineConfig({});
--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'AS-SELFHOSTABLE';
-export const SITE_DESCRIPTION = 'Information Center for the AS-SET AS197532:AS-SELFHOSTABLE.';
